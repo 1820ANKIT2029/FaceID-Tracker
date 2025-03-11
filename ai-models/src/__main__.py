f557cc4ba3181d7fe134e5f899e56edf885eecf3
1820ANKIT2029 <ankitkumar00002250908@gmail.com>
2025-03-12 02:17:43 +0530

    cmd for project

---
+++ ai-models/src/__main__.py
@@ -0,0 +1,61 @@
+import sys
+
+from .config import config
+
+flags = [
+    "--help", "--download", "--model", "--saved-model", "--train-new-model", "--train-model",
+    "--image-capture"
+]
+
+tf_load = [
+    "--model", "--saved-model", "--train-new-model", "--train-model"
+]
+
+for i in tf_load:
+    if i in sys.argv:
+        from .training.modelutils import model_detail, saved_model_detail, training
+        break
+
+
+def main():
+    argv = sys.argv
+
+    if len(argv) == 1:
+        print("python -m src <flags>")
+        print("flags: ", flags)
+        return
+
+    if "--download" in argv:
+        from .utils import download_data
+
+        dataset_url = config["dataset_url"]
+        data_folder = config["data_folder"]
+
+        download_data(dataset_url, data_folder)
+
+    if "--model" in argv:
+        model_detail()
+
+    if "--saved-model" in argv:
+        file = saved_model_detail()
+        
+    if "--train-new-model" in argv:
+        historylist = training()
+
+        # show_plot(historylist)
+
+    if "--train-model" in argv:
+        file = saved_model_detail()
+
+        historylist = training(file)
+
+        # show_plot(historylist)
+
+    if "--image-capture" in argv:
+        from .img_capture import save_archor_positive_image
+        
+        save_archor_positive_image()
+
+
+if __name__ == "__main__":
+    main()
