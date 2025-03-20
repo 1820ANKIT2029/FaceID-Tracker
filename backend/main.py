0ac28590a25246f79cc2dc1dae37aa31bf306797
Anandank <ankitkumaranand68@gmail.com>
2025-03-21 00:06:03 +0530

    Modified predict route to take multiple images

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, status
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from typing import List
 import cv2
 import numpy as np
 from ai_models.src.inference.Manager import ModelManager
 from ai_models.src.config import config
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -20,21 +20,27 @@ app.add_middleware(
 # Instantiated ModelManager 
 model_manager = ModelManager(model_path=config["save_model_folder"], global_search=False)
 
-
-app = FastAPI()
-
 def read_image_as_numpy(file: UploadFile) -> np.ndarray:
     """Convert an uploaded image file to a NumPy array."""
     contents = file.file.read()
-    np_array = np.frombuffer(contents, np.uint8) 
+    np_array = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
     return img
 
-@app.post("/predict/")  
-async def predict(file: UploadFile = File(...)):
-    img_array = read_image_as_numpy(file) 
-    prediction_result = model_manager.local_prediction(img_array)
-
+@app.post("/predict/")
+async def predict(files: List[UploadFile] = File(...)):
+    if not files:
+        raise HTTPException(status_code=400, detail="No files uploaded")
+    
+    prediction_results = []
+    for file in files:
+        try:
+            img_array = read_image_as_numpy(file)
+            prediction = model_manager.local_prediction(img_array)
+            prediction_results.append(prediction)
+        except Exception as e:
+            prediction_results.append({"error": str(e)})
+    print(prediction)
     return {
-        "prediction_result": prediction_result
+        "prediction_result": prediction_results
     }
