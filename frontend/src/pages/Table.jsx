f52e3a69fc4bad1af0ed7c23e09fafb9fb6df991
aloknegi786 <aloksinghnegi005@gmail.com>
2025-04-24 00:50:03 +0530

    removed the bug on file table related rendering each item

--- frontend/src/pages/Table.jsx
+++ frontend/src/pages/Table.jsx
@@ -25,7 +25,7 @@ function Table({ data }) {
                 className="hover:bg-cyan-500/10 transition duration-200 border-t border-cyan-800"
               >
                 <td className="py-2 px-6 font-medium">{item.name}</td>
-                <td className="py-2 px-6">{item.confidence.toFixed(2)}%</td>
+                <td className="py-2 px-6">{item?.confidence?.toFixed(2)}%</td>
               </tr>
             ))
           )}
